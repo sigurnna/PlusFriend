f09bc739a56c300eb5df7ff3b5cf08dddad33918
seungjun.lee <sigurnna@gmail.com>
2018-06-07 16:01:25 +0900

    - cheerio npm 추가. 원주 캠퍼스 식단 주소로 get 요청하는 코드 추가.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,8 @@
 // Import Module
 const functions = require('firebase-functions');
-const express = require('express');
 const session = require('express-session');
-
-const app = express();
+const http = require('http');
+const webparser = require('./webparser');
 
 // First Appear Menu
 const wonjuCampus = "오늘의 중식 메뉴(원주)";
@@ -11,14 +10,8 @@ const gangreungCampus = "오늘의 중식 메뉴(강릉)";
 
 const ganreungMenu = ['제 1 학생식당 중식백반', '제 2 학생식당 일품요리', '문화관식당'];
 
-app.use(session({
-    secret: 'sigurnna',
-    resave: false,
-    saveUninitialized: true
-}));
-
 // Kakao Plus Friend Initial Endpoint.
-app.get('/keyboard', (req, res) => {
+exports.keyboard = functions.https.onRequest((req, res) => {
     res.append('Content-type', 'application/json; charset=utf-8');
     res.json({
         'type': 'buttons',
@@ -27,27 +20,59 @@ app.get('/keyboard', (req, res) => {
 });
 
 // 사용자가 플러스친구 채팅에 입력한 내용이 항상 여기로 도착함.
-app.post('/message', (req, res) => {
+exports.message = functions.https.onRequest((req, res) => {
+    if (req.method != 'POST') { 
+        res.status(400);
+        return;
+    }
+
     req.accepts('application/json');
     req.acceptsCharsets('utf8');
 
+    const content = req.body['content'];
+
     // TODO: gangreungMenu 메시지가 도착한 경우 processMenuSelection으로 라우트 되도록 하자.
-    if (content == wonjuCampus|| content == gangreungCampus) {
+    if (content == wonjuCampus || content == gangreungCampus) {
         processCampusSelection(req, res);
     } else {
-        res.status(400);
+        console.log('Unexpected campus name arrived: ' + content);
+        res.status(400).end();
     }
 });
 
 // Menu Selection
 
 function processCampusSelection(req, res) {
+    console.log('processCampusSelection');
     const campus = req.body['content'];
 
     if (campus == wonjuCampus) {
-        // TODO: 원주 크롤링 진행.
+        const url = "http://www.gwnu.ac.kr/kor/251/subview.do";
+
+        http.get(url, (gwnuRes) => {
+            if (gwnuRes.statusCode != 200) { 
+                res.status(gwnuRes.statusCode);
+                return;
+            }
+
+            let rawData = '';
+
+            gwnuRes.setEncoding('utf8');
+            gwnuRes.on('data', (chunk) => { 
+                rawData += chunk; 
+            });
+
+            gwnuRes.on('end', () => {
+                // TODO: 원주 메뉴 파싱 진행.
+            });
+
+            gwnuRes.on('error', (e) => {
+                res.send(e.message);
+            });
+        });
     } else if (campus == gangreungCampus) {
         // TODO: 강릉은 메뉴를 다시 선택해야 함. buttons를 리턴하자.
+        res.status(400);
     } else {
         res.status(400);
     }
@@ -56,7 +81,3 @@ function processCampusSelection(req, res) {
 function processMenuSelection(req, res) {
 
 }
-
-// Crwaling
-
-exports.staging = functions.https.onRequest(app);
---
+++ functions/webparser/webparser.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const cheerio = require('cheerio');
+
+/* return object
+{
+    result: 'success' or 'fail',
+    menus: [
+        {
+            date: '2018.06.07 (월)',
+            menu_name: '치즈치킨돌솥비빔밥'
+        },
+        {
+            date: '2018.06.07 (화)',
+            menu_name: '제육덮밥'
+        }
+    ]
+}
+*/
+
+function parseWonjuMenu() {
+    
+}
+
+function parseGangreungMenu() {
+    
+}

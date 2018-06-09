4f9975f3fca989685575d49aaad4edd7bb141573
seungjun.lee <sigurnna@gmail.com>
2018-06-10 01:10:57 +0900

    - 식단을 불러오는 로직 변경
    - 서버 최초 실행 시, 하루가 지날 떄 마다 식단을 불러오도록 수정.
    - firebase functions의 느린 latency로 인하여 미리 식단 데이터를 받아와야 함.

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,21 @@
 const functions = require('firebase-functions');
 const http = require('http');
 const cheerio = require('cheerio');
+const admin = require('firebase-admin');
 
 // First Appear Menu
 const wonjuCampus = "오늘의 중식 메뉴(원주)";
 const gangreungCampus = "오늘의 중식 메뉴(강릉)";
 
-const ganreungMenu = ['제 1 학생식당 중식백반', '제 2 학생식당 일품요리', '문화관식당'];
+const gangreungMenu = ['제 1 학생식당 중식백반', '제 2 학생식당 일품요리', '문화관식당'];
+
+// 각 캠퍼스별 메뉴를 메모리에 저장함.
+const campusMenu = {
+    wonju: [],
+    gangreung: []
+};
+
+fetchCampusMenu();
 
 // Kakao Plus Friend Initial Endpoint.
 exports.keyboard = functions.https.onRequest((req, res) => {
@@ -39,83 +48,52 @@ exports.message = functions.https.onRequest((req, res) => {
     }
 });
 
-// Menu Selection
+// Timer for fetching campus menu
 
-function processCampusSelection(req, res) {
-    const campus = req.body['content'];
+function fetchCampusMenu() {
+    console.log("fetching campus menus...");
 
-    if (campus === wonjuCampus) {
-        console.log("User select wonju campus");
+    fetchWonjuCampusMenu((result) => {
+        if (result) {
+            const today = new Date();
+            const triggerDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1, 0, 0, 0);
+        
+            setTimeout(fetchCampusMenu, triggerDate - today);
+        } else {
+            console.error("fetching wonju campus menu failed");
+        }
+    });
+}
 
-        const url = "http://www.gwnu.ac.kr/kor/251/subview.do";
+function fetchWonjuCampusMenu(onComplete) {
+    http.get("http://www.gwnu.ac.kr/kor/251/subview.do", (res) => {
+        console.log("Send request to wonju campus");
 
-        http.get(url, (gwnuRes) => {
-            console.log("Send request to wonju campus");
+        if (res.statusCode !== 200) { 
+            console.log("Receive unexpected status code from wonju campus");
+            onComplete(false);
+        }
 
-            if (gwnuRes.statusCode !== 200) { 
-                console.log("Response from wonju campus failed");
-                res.status(gwnuRes.statusCode);
-                return;
-            }
+        let rawData = '';
 
-            let rawData = '';
-
-            gwnuRes.setEncoding('utf8');
-            gwnuRes.on('data', (chunk) => { 
-                rawData += chunk; 
-            });
-
-            gwnuRes.on('end', () => {
-                const menus = parseWonjuMenu(rawData);
-                const todayDate = getCurrentDateString();
-
-                let todayMenuNames = null;
-
-                for (menu in menus) {
-                    if (menu.date === todayDate) {
-                        todayMenuNames = menu.menu_names;
-                        break;
-                    }
-                }
-
-                res.append('Content-type', 'application/json; charset=utf-8');
-
-                if (todayMenuNames !== null) {
-                    console.log("Send response to PlusFriend with menu names");
-                    res.json({
-                        "message": {
-                            "text": todayMenuNames
-                        }
-                    });
-                } else {
-                    console.log("Send response to PlusFriend with empty menu");
-                    res.json({
-                        "message": {
-                            "text": "오늘은 중식 메뉴가 없습니다."
-                        }
-                    });
-                }
-            });
-
-            gwnuRes.on('error', (e) => {
-                res.status(500).send(e.message);
-            });
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => { 
+            rawData += chunk; 
         });
-    } else if (campus === gangreungCampus) {
-        // TODO: 강릉은 메뉴를 다시 선택해야 함. buttons를 리턴하자.
-        res.status(400);
-    } else {
-        res.status(400);
-    }
-}
 
-function processMenuSelection(req, res) {
+        res.on('end', () => {
+            console.log("Receive all response from wonju campus");
 
-}
+            campusMenu.wonju = parseWonjuMenu(rawData);
 
-function getCurrentDateString() {
-    const date = new Date();
-    return date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
+            onComplete(true);
+        });
+
+        res.on('error', (e) => {
+            console.log("Receive error from wonju campus");
+            onComplete(false);
+        });
+    });
 }
 
 // Web parsing
@@ -156,4 +134,45 @@ function parseWonjuMenu(rawHTML) {
 
 function parseGangreungMenu() {
     
+}
+
+// Menu Selection
+
+function processCampusSelection(req, res) {
+    const campus = req.body['content'];
+
+    if (campus === wonjuCampus) {
+        console.log("User select wonju campus");
+
+        let todayMenu = null;
+
+        for (menu in campusMenu.wonju) {
+            if (menu.date == getCurrentDateString()) {
+                todayMenu = menu;
+            }
+        }
+
+        let resMsg = (todayMenu != null) ? todayMenu : "오늘은 식단이 없네요!!";
+
+        res.json({
+            message: {
+                text: resMsg
+            }
+        });
+    } else if (campus === gangreungCampus) {
+        console.log("User select gareung campus");
+        // TODO: 강릉은 메뉴를 다시 선택해야 함. buttons를 리턴하자.
+        res.status(400);
+    } else {
+        res.status(400);
+    }
+}
+
+function processMenuSelection(req, res) {
+
+}
+
+function getCurrentDateString() {
+    const date = new Date();
+    return date.getFullYear() + "." + (date.getMonth() + 1) + "." + date.getDate();
 }
